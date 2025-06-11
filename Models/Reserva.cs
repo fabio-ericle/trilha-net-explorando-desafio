c85d746048ca6bfde7e39354c91d8c96da98cceb
fabio-ericle <fabioericle@gmail.com>
2025-06-11 17:42:25 -0300

    Fix: Adicionado funcionalidade

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (hospedes.Count > 0 && hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception($"A quantida de hospedes informado ({hospedes.Count}) excede a capacidade limite suportada ({Suite.Capacidade}) da suite!");
             }
         }
 
@@ -35,23 +32,19 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                // O mesmo de retorno que: 
+                // [valor * 10/100 = resultado], resultado é a quantidade de 10% do valor -> 
+                // [valor = valor - resultado], Valor recebe a quantia com os 10% de desconto
+                valor *= 0.9M;
             }
 
             return valor;
